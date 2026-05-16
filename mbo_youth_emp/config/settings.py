5a995f2810175827a15d03df3b8af30f90d96ed8
EDMUND ANTAI ANTAI <160632736+Devonlegend@users.noreply.github.com>
2026-05-16 22:49:57 +0100

    Update SECRET_KEY handling in settings.py
    Replaced SECRET_KEY loading from environment variable with a generated secret key.

--- mbo_youth_emp/config/settings.py
+++ mbo_youth_emp/config/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-import os
-from dotenv import load_dotenv
+
+
 from pathlib import Path
-load_dotenv()
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
